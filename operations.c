5d63ae8763ce7574e620de44b2d59839f73d1cf5
Lohitha Sri <smiley451745@gmail.com>
2025-12-15 21:42:59 +0530

    Add files via upload

---
+++ operations.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("ENTER A AND B VALUES:");
+	scanf("%d %d",&a,&b);
+	system("cls");
+	printf("ADDITION OF A AND B IS:%d\n",a+b);
+	printf("SUBTRACTION OF A AND B VALUES IS:%d\n",a-b);
+	printf("MULTIPLICATION OF A AND B VALUES IS:%d\n",a*b);
+	printf("MODULO OF A AND B IS:%d\n",a%b);
+}
