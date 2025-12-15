1f8d854aece68e54f759d7b499dd398f672131aa
Lohitha Sri <smiley451745@gmail.com>
2025-12-15 21:24:52 +0530

    Add files via upload

---
+++ addof matrix.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int a[100][100],b[100][100],C[100][100],r,c,i,j;
+	printf("ENTER THE SIZE OF ROWS AND COLUMNS:");
+	scanf("%d %d",&r,&c);
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			printf("\nENTER VALUE AT A a[%d][%d] POSITION:",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			printf("\nENTER THE VALUES OF b[%d][%d]:",i,j);
+			scanf("%d",&b[i][j]);
+		}
+	}
+		for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+		C[i][j]=a[i][j]+b[i][j];
+		printf("\t%d",C[i][j]);		
+	}
+	printf("\n");		
+		}
+	return 0;}
