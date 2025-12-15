1ba8b353585993bfa3b6464471cd565e672249e9
Lohitha Sri <smiley451745@gmail.com>
2025-12-15 21:37:38 +0530

    Add files via upload

---
+++ lohi7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fib(int n){
+	if(n==0){
+		return 0;
+	}
+	else if(n==1){
+		return 1;
+	}
+	else 
+	return fib(n-2)+fib(n-1);
+}
+int main(){
+	int i,n;
+	printf("ENTER N VALUE:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		printf("%d\t",fib(i));
+	}
+}
