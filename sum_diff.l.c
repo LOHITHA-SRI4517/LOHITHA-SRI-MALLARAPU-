0a79c6631b577f3a0aaf840fcddc96114858e76d
Lohitha Sri <smiley451745@gmail.com>
2025-12-15 21:49:53 +0530

    Add files via upload

---
+++ sum_diff.l.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("ENTER A AND B VALUES:");
+	scanf("%d %d",&a,&b);
+	printf("\nSUM OF VALUES OF A AND B IS:%d",a+b);
+	printf("\nDIFFERENCE OF VALUES A AND B IS :%d",a-b);
+	return 0;
+}
